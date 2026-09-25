6407c709b460c5b7a4251cdf055c0c5cf7536a4a
EvDemV <2907i1609@gmail.com>
2026-09-25 13:28:55 +0700

    #2 commit добавление функции отображения истории аккаунта

--- Bank/Bank/BankAccount.cs
+++ Bank/Bank/BankAccount.cs
@@ -1,4 +1,6 @@
-﻿namespace Bank;
+﻿using System.Text;
+
+namespace Bank;
 
 internal class BankAccount
 { 
@@ -58,3 +60,15 @@ internal class BankAccount
 
 
 }
+public string GetAccountHistory()
+    {
+        var report = new StringBuilder();
+
+        decimal balance = 0;
+        report.AppendLine("Data\t\tAmount\tBalance\tNote");
+        foreach (var item in _allTransactions) {
+            balance += item.Amount;
+            report.AppendLine($"" + $"{item.Date.ToShortDateString()}\t" + $"{item.Amount}\t{balance}\t{item.Note}");
+        }
+        return report.ToString();
+    }
